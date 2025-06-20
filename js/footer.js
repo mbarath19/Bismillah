b4b9618e92fa85abfc545ed96bb041037e983455
BARATH M <mabarth385@gmail.com>
2025-06-20 14:55:08 +0530

    commit

---
+++ js/footer.js
@@ -0,0 +1,71 @@
+const footer = `
+<footer class="ftco-footer ftco-section img">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">About Us</h2>
+                    <p>A destination for non-vegetarian food lovers, offering a menu that celebrates meat in all its forms, from classic steaks to exotic seafood dishes.</p>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="https://www.instagram.com/bismillah_restaurantz?igsh=dTdleDdvazB2dDA1"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 mb-md-5">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Recent Blog</h2>
+                    <div class="block-21 mb-4 d-flex">
+                        <div class="text">
+                            <h3 class="heading"><a href="#">"Want to recreate our famous Biriyani at home? Here's the recipe straight from our kitchen. "</a></h3>
+                            <div class="meta"></div>
+                        </div>
+                    </div>
+                    <div class="block-21 mb-4 d-flex">
+                        <div class="text">
+                            <h3 class="heading"><a href="#">"We believe in supporting local farmers and using fresh, local ingredients. "</a></h3>
+                            <div class="meta"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 mb-5 mb-md-5">
+                <div class="ftco-footer-widget mb-4 ml-md-4">
+                    <h2 class="ftco-heading-2">Services</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="py-2 d-block">Cooked</a></li>
+                        <li><a href="#" class="py-2 d-block">Deliver</a></li>
+                        <li><a href="#" class="py-2 d-block">Quality Foods</a></li>
+                        <li><a href="#" class="py-2 d-block">Mixed</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text"> No.10, Mariyamman Kovil 10th Street, Tiruvannamalai, Tamilnadu 606601</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">95974 54311</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text"> shajahanbismillahhotel@gmail.com</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;
+                    <script>document.write(new Date().getFullYear());</script> All rights reserved | by <a href="https://corpwings.in" target="_blank">Corpwings</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+`;
+
+document.write(footer);
